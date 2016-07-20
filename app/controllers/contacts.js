6bb3e26a06dc66657cf367b33d32030453792cb8
Adam Hillier <dev@adamhillier.eu>
2016-07-20 13:50:44 +0100

    Update contact search to show (with an icon) which field is being searched and to be able to override the automatically detected field.

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -1,9 +1,12 @@
 'use strict';
-angular.module('civimobile').controller('ContactsController', ['$state', '$stateParams', 'ApiService', 'ngDialog', function ($state, $stateParams, ApiService, ngDialog) {
+angular.module('civimobile').controller('ContactsController', ['$state', 'ApiService', 'ngDialog', function ($state, ApiService, ngDialog) {
     this.contacts = [];
     this.query = '';
     this.loading = 0; // Number of ongoing 'searches', 0 => loaded
     this.geoHeader = '';
+    this.searchFields = ['name', 'email', 'phone'];
+    this.searchField = this.searchFields[0];
+    this.manual = false;
 
     // Lets us refer to 'this' in promises.
     var x = this;
@@ -17,28 +20,49 @@ angular.module('civimobile').controller('ContactsController', ['$state', '$state
         }
     }
 
+    this.changeField = function (newField, setManual) {
+        if (setManual) { this.manual = true; };
+        if (newField != this.searchField) {
+            this.searchFields = [newField];
+            if ('name' != newField) { this.searchFields.push('name'); }
+            if ('email' != newField) { this.searchFields.push('email'); }
+            if ('phone' != newField) { this.searchFields.push('phone'); }
+            this.searchField = newField;
+            this.search();
+        }
+    }
+
     this.search = function () {
-        var emailRegex = /^\S+@\S+$/;
-        var phoneRegex = /[0-9]{5,15}/;
+        if (!this.manual) {
+            var emailRegex = /^\S+@\S+$/;
+            var phoneRegex = /[0-9]{5,15}/;
+
+            if (emailRegex.test(this.query)) {
+                this.changeField('email');
+            } else if (this.query.replace(/\d/g,'').length < 4 && phoneRegex.test(this.query.replace(/\D/g,''))) {
+                this.changeField('phone');
+            } else {
+                this.changeField('name');
+            }
+        }
 
         var q = this.query;
-        var searchField;
-        if (emailRegex.test(this.query)) {
-            searchField = 'email';
-        } else if (this.query.replace(/\d/g,'').length < 4 && phoneRegex.test(this.query.replace(/\D/g,''))) {
-            searchField = 'phone';
+        if (x.searchField == 'phone') {
             // Remove inital zero if there is one and replace special chars with a wildcard to match as many
             // different phone number formats as possible.
             q = {
                 LIKE: '%' + this.query.replace(/^[0]/, '').replace(/[ #+.-]{1,}/g, '%') + '%'
             };
-        } else {
-            searchField = 'display_name';
+        }
+
+        var field = x.searchField;
+        if (field == 'name') {
+            field = 'display_name';
         }
 
         this.loading += 1;
         this.contacts = [];
-        ApiService.contactSearch(q, searchField).then(function (data) {
+        ApiService.contactSearch(q, field).then(function (data) {
             if (x.loading > 0) {    // Necessary as geolocation may set loading to 0 directly.
                 x.loading -= 1;
             }
